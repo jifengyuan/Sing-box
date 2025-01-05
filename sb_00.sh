002646852bcfc00e9d68498c4c877ee8bf7bcb0c
jifengyuan <yuanfeng52022@gmail.com>
2025-01-05 20:53:55 +0800

    Update sb_00.sh

--- sb_00.sh
+++ sb_00.sh
@@ -13,7 +13,7 @@ reading() { read -p "$(red "$1")" "$2"; }
 export LC_ALL=C
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-export UUID=${UUID:-'bc97f674-c578-4940-9234-0a1da46041b0'}
+export UUID=${UUID:-'901d361d-9e3a-45e8-ae69-46634a1c8c8e'}
 export NEZHA_SERVER=${NEZHA_SERVER:-''} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''} 
